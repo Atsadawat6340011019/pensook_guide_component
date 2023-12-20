3bf4c1ca3efcc423120e6e5d6a9e77d3d375b1c8
Atsadawat6340011019 <atsadawat.kontha@gmail.com>
2023-12-20 09:45:03 +0700

    delete file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
 import "./App.css";
-import Boat1 from "./component/learnRedux/Boat1";
-import Boat2 from "./component/learnRedux/Boat2";
-
-import { PostStatus } from "./component/PostStatus";
-
 import Guide_Search from "./component/DialogGuide_FirsttimeLogin/Guide_Search";
 
 function App() {
